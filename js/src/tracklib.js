d004043a423824b12c7eed90f759713ec38f18b0
Luis <luileito@varial.(none)>
2012-06-01 13:59:10 +0200

    Initial commit (v0.2)

---
+++ js/src/tracklib.js
@@ -0,0 +1,367 @@
+/*! evtrack -- Lib module */
+
+/**
+ * Auxiliary functions to track the user activity.
+ * @author Luis Leiva
+ * @version 0.2
+ * @license Dual licensed under the MIT and GPL licenses.
+ */
+var TrackLib = {};
+/**
+ * XPath functions.
+ * Not documented yet.
+ * Code extracted from window.js @ http://code.google.com/p/xpathchecker/ 
+ */
+TrackLib.XPath = {
+
+    queryXPath: function(document, xpath) {
+      var xpathResult;
+      if (typeof document.evaluate === 'function') {
+        xpathResult = document.evaluate(xpath, document.documentElement, null, XPathResult.ANY_TYPE, null);
+      } else {
+        try {
+          // IE5 and later has implemented that [0] should be the first node, 
+          // but according to the W3C standard it should have been [1]!
+          document.setProperty("SelectionLanguage", "XPath");
+          xpathResult = document.selectNodes(xpath);
+        } catch(err) {
+          xpathResult = false;
+        }
+      }
+      
+      return xpathResult;
+    },
+    
+    getXPathNodes: function(document, xpath) {
+      var xpathResult = this.queryXPath(document, xpath);
+      var result = [];
+      var item = xpathResult.iterateNext();
+      while (item) {
+        result.push(item);
+        item = xpathResult.iterateNext();
+      }
+      
+      return result;
+    },
+
+    getXPath: function(targetNode) {
+      var useLowerCase = (targetNode.ownerDocument instanceof HTMLDocument);
+      var nodePath = this.getNodePath(targetNode);
+      var nodeNames = [];
+      for (var i in nodePath) {
+        var nodeIndex;
+        var node = nodePath[i];
+        if (node.nodeType == 1) { // && node.tagName != "TBODY") {
+          if (i == 0 && node.hasAttribute("id")) {
+            nodeNames.push("/*[@id='" + node.getAttribute("id") + "']");
+          } else {
+            var tagName = node.tagName;
+            if (useLowerCase) {
+              tagName = tagName.toLowerCase();
+            }
+            nodeIndex = this.getNodeIndex(node);
+            if (nodeIndex != null) {
+              nodeNames.push(tagName + "[" + nodeIndex + "]");
+            } else {
+              nodeNames.push(tagName);
+            }
+          }
+        } else if (node.nodeType == 3) {
+          nodeIndex = this.getTextNodeIndex(node);
+          if (nodeIndex != null) {
+            nodeNames.push("text()[" + nodeIndex + "]");
+          } else {
+            nodeNames.push("text()");
+          }
+        }
+      }
+      
+      return "/" + nodeNames.join("/");
+    },
+
+    getNodeIndex: function(node) {
+      if (node.nodeType != 1 || node.parentNode == null) return null;
+      var list = this.getChildNodesWithTagName(node.parentNode, node.tagName);
+      if (list.length == 1 && list[0] == node) return null;
+      for (var i = 0; i < list.length; i++) {
+        if (list[i] == node) return i + 1;
+      }
+      
+      throw "couldn't find node in parent's list: " + node.tagName;
+    },
+
+    getTextNodeIndex: function(node) {
+      var list = this.getChildTextNodes(node.parentNode)
+      if (list.length == 1 && list[0] == node) return null;
+      for (var i = 0; i < list.length; i++) {
+        if (list[i] == node) return i + 1;
+      }
+      
+      throw "couldn't find node in parent's list: " + node.tagName;
+    },
+
+    getChildNodesWithTagName: function(parent, tagName) {
+      var result = [];
+      var child = parent.firstChild;
+      while (child != null) {
+        if (child.tagName && child.tagName == tagName) {
+          result.push(child);
+        }
+        child = child.nextSibling;
+      }
+      
+      return result;
+    },
+
+    getChildTextNodes: function(parent) {
+      var result = [];
+      var child = parent.firstChild;
+      while (child != null) {
+        if (child.nodeType == 3) {
+          result.push(child);
+        }
+        child = child.nextSibling;
+      }
+      
+      return result;
+    },
+
+    getNodePath: function(node) {
+      var result = [];
+      while (node.nodeType == 1 || node.nodeType == 3) {
+        result.unshift(node);
+        if (node.nodeType == 1 && node.hasAttribute("id")) return result;
+        node = node.parentNode;
+      }
+      
+      return result;
+    },
+    
+    getNodeValues: function(resultList) {
+      var result = [];
+      for (var i in resultList) {
+        result.push(resultList[i].nodeValue);
+      }
+      
+      return result;
+    }
+  
+};
+/**
+ * Ajax handling object.
+ */
+TrackLib.XHR = {
+  /**
+   * Creates an XML/HTTP request to provide async communication with the server.
+   * @return {object} XHR object
+   * @autor Peter-Paul Koch (quirksMode.org)
+   */
+  createXMLHTTPObject: function() {
+    var xmlhttp = false;
+    // current AJAX flavours
+    var factories = [
+      function(){ return new XMLHttpRequest(); },
+      function(){ return new ActiveXObject("Msxml2.XMLHTTP"); },
+      function(){ return new ActiveXObject("Msxml3.XMLHTTP"); },
+      function(){ return new ActiveXObject("Microsoft.XMLHTTP"); }
+    ];
+    // check AJAX flavour
+    for (var i = 0; i < factories.length; ++i) {
+      try {
+        xmlhttp = factories[i]();
+      } catch(e) { continue; }
+      break;
+    }
+
+    return xmlhttp;
+  },
+  /**
+   * Makes an asynchronous XMLHTTP request (XHR) via GET or POST.
+   * Inspired by Peter-Paul Koch's XMLHttpRequest function.
+   * Note: CORS on IE will work only for version 8 or higher.
+   * @return void
+   * @param  {object} setup Request properties
+   *    @config {string}    url           Request URL
+   *    @config {boolean}  [async]    Asynchronous request (or not)
+   *    @config {function} [callback] Response function
+   *    @config {string}   [postdata] POST vars in the form "var1=name&var2=name..."
+   *    @config {object}   [xmlhttp]  A previous XMLHTTP object can be reused
+   */
+  sendAjaxRequest: function(setup) {
+    // create XHR object or reuse it
+    var request = setup.xmlhttp ? setup.xmlhttp : this.createXMLHTTPObject();
+    var cors = !TrackLib.Util.sameDomain(window.location.href, setup.url);
+    // CORS does work with XMLHttpRequest on modern browser, except IE
+    if (cors && window.XDomainRequest) request = new XDomainRequest();
+    if (!request) return false;
+    
+    var method = setup.postdata ? "POST" : "GET";
+    var asynchronous = setup.hasOwnProperty('async') ? setup.async : true;
+    // start request
+    request.open(method, setup.url, asynchronous);
+    
+    var iecors = window.XDomainRequest && (request instanceof XDomainRequest);
+    // post requests must set the correct content type (not allowed under CORS + IE, though)
+    if (setup.postdata && !iecors) 
+      request.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
+
+    // add load listener
+    if (iecors) {
+      request.onload = function(){
+        if (typeof setup.callback === 'function') setup.callback(request.responseText);
+      };
+    } else {
+      // check for the 'complete' request state
+      request.onreadystatechange = function(){
+        if (request.readyState == 4 && typeof setup.callback === 'function') {
+          // send server response to callback function
+          setup.callback(request.responseText);
+        }
+      };
+    }
+    // send request
+    request.send(setup.postdata);
+  }
+  
+};
+/**
+ * Event handling object.
+ */
+TrackLib.Events = {
+    /**
+     * Adds event listeners unobtrusively.
+     * @author John Resig http://ejohn.org
+     * @param {object}    obj   Object to add listener(s) to.
+     * @param {string}    type  Event type.
+     * @param {function}  fn    Function to execute.
+     * @return void
+     */  
+    add: function(obj, type, fn) {
+      if (!obj) return false;
+      if (obj.addEventListener) { // W3C standard
+        obj.addEventListener(type, fn, false);
+      } else if (obj.attachEvent)	{ // IE versions
+        obj["e"+type+fn] = fn;
+        obj[type+fn] = function(){ obj["e"+type+fn](window.event); };
+        obj.attachEvent("on"+type, obj[type+fn]);
+      }
+    },
+    /**
+     * Removes event listeners unobtrusively.
+     * @author John Resig http://ejohn.org     
+     * @param {object}    obj   Object to remove listener(s) from
+     * @param {string}    type  Event type
+     * @param {function}  fn    Function to remove from event
+     * @return void
+     */    
+    remove: function(obj, type, fn) {
+      if (!obj) return false;
+      if (obj.removeEventListener) { // W3C standard
+        obj.removeEventListener(type, fn, false);
+      } else if (obj.detachEvent)	{ // IE versions
+        obj.detachEvent("on"+type, obj[type+fn]);      
+        obj[type+fn] = null;
+      }
+    }, 
+    /**
+     * Fixes event handling inconsistencies between browsers.
+     * @param {object}  e Event
+     * @return {object}   Fixed event     
+     */
+    fix: function(e) {
+      e = e || window.event;
+      // fix target property, if necessary (IE 6/7/8 & Safari 2)
+      if (!e.target) e.target = e.srcElement || document;
+      // target should not be a text node (Safari bug)
+      if (e.target.nodeType == 3) e.target = e.target.parentNode;
+      // for mouse/key events; add metaKey if it's not there (IE 6/7/8)
+  		if (typeof e.metaKey === 'undefined') e.metaKey = e.ctrlKey;
+
+      return e;
+    }
+    
+};
+/**
+ * Dimension handling object.
+ */
+TrackLib.Dimension = {
+  /**
+   * Gets the browser's window size (aka 'the viewport').
+   * @return {object} window dimmensions - object with 2 properties: width {integer}, and height {integer}
+   */
+  getWindowSize: function() {
+    var d = document;
+    var w = (window.innerWidth) ? window.innerWidth
+            : (d.documentElement && d.documentElement.clientWidth) ? d.documentElement.clientWidth
+            : (d.body && d.body.clientWidth) ? d.body.clientWidth
+            : 0;
+    var h = (window.innerHeight) ? window.innerHeight
+            : (d.documentElement && d.documentElement.clientHeight) ? d.documentElement.clientHeight
+            : (d.body && d.body.clientHeight) ? d.body.clientHeight
+            : 0;
+    return { width: w, height: h };
+  },
+  /**
+   * Gets the document's size.
+   * @return {object} document dimensions - object with 2 properties: width {integer}, and height {integer}
+   */
+  getDocumentSize: function() {
+    var d = document;
+    var w = (window.innerWidth && window.scrollMaxX) ? window.innerWidth + window.scrollMaxX
+            : (d.body && d.body.scrollWidth > d.body.offsetWidth) ? d.body.scrollWidth
+            : (d.body && d.body.offsetWidth) ? d.body.offsetWidth
+            : 0;
+    var h = (window.innerHeight && window.scrollMaxY) ? window.innerHeight + window.scrollMaxY
+            : (d.body && d.body.scrollHeight > d.body.offsetHeight) ? d.body.scrollHeight
+            : (d.body && d.body.offsetHeight) ? d.body.offsetHeight
+            : 0;
+
+    return { width: w, height: h };
+  },
+  /**
+   * Gets the max value from both window (viewport's size) and document's size.
+   * @return {object} viewport dimensions - object with 2 properties: width {integer}, and height {integer}
+   */
+  getPageSize: function()
+  {
+    var win = this.getWindowSize(),
+        doc = this.getDocumentSize();
+
+    // find max values from this group
+    var w = (doc.width < win.width) ? win.width : doc.width;
+    var h = (doc.height < win.height) ? win.height : doc.height;
+
+    return { width: w, height: h };
+  }
+
+};
+/**
+ * Some utilies.
+ */
+TrackLib.Util = {
+  /**
+   * Tests whether a set of URLs come from the same domain.
+   * @return {boolean}
+   */
+  sameDomain: function() {
+    var prevDomain, sameDomain = true;
+    for (var i = 0, l = arguments.length; i < l; ++i) {
+      if (i > 0) {
+        sameDomain = (this.getDomain(prevDomain) == this.getDomain(arguments[i]));
+      }
+      prevDomain = arguments[i];
+    }
+
+    return sameDomain;
+  },
+  /**
+   * Gets the domain of a given URL.
+   * @return {string}
+   */
+  getDomain: function(url) {
+    var domain = url.split(/\/+/g)[1];
+    // this test is necessary for local URLs
+    return (typeof d === 'undefined') ? window.location.href : domain;
+  }
+
+};
---
+++ js/src/trackui.js
@@ -0,0 +1,222 @@
+/*! evtrack -- UI module */
+
+/**
+ * A small lib to track the user activity by listening to browser events.
+ * @author Luis Leiva
+ * @version 0.2
+ * @requires tracklib.js
+ * @license Dual licensed under the MIT and GPL licenses.
+ */
+var TrackUI = {
+  /** 
+   * Default settings -- can be overriden on init.
+   */
+  settings: {
+    postServer: "http://my.server.org/save.script",
+    postInterval: 10
+  }, 
+  /** 
+   * Unique user ID.
+   */
+  uid: 0, 
+  /** 
+   * Registered information is timestamp, xpos, ypos, event, element
+   */
+  info: [],
+  /** 
+   * Registers event listeners.
+   * @return void
+   */
+  record: function(config) {
+    // override, if need be
+    for (var prop in this.settings) if (config.hasOwnProperty(prop) && config[prop] !== null) {
+      this.settings[prop] = config[prop];
+    }
+
+    var mouseEvts = ["mousedown", "mouseup", "mousemove", "click", "scroll", "mousewheel"],
+        touchEvts = ["touchstart", "touchend", "touchmove"],
+        keyEvts = ["keydown", "keyup", "keypress"],
+        winEvts = ["blur", "focus", "resize"],
+        i;
+        
+    for (i = 0; i < mouseEvts.length; ++i) TrackLib.Events.add(document, mouseEvts[i], TrackUI.mouseHandler);
+    for (i = 0; i < touchEvts.length; ++i) TrackLib.Events.add(document, touchEvts[i], TrackUI.touchHandler);
+    for (i = 0; i < keyEvts.length; ++i) TrackLib.Events.add(document, keyEvts[i], TrackUI.keyHandler);
+    for (i = 0; i < winEvts.length; ++i) TrackLib.Events.add(window, winEvts[i], TrackUI.winHandler);
+    // this is for IE compatibility, grrr
+    if (document.attachEvent) {
+      // see http://todepoint.com/blog/2008/02/18/windowonblur-strange-behavior-on-browsers/
+      TrackLib.Events.add(document.body, "focusout", TrackUI.winHandler);
+      TrackLib.Events.add(document.body, "focusin",  TrackUI.winHandler);
+    }
+    setTimeout(TrackUI.initNewData, config.postInterval*1000);
+
+    var unload = (typeof window.onbeforeunload === 'function') ? "beforeunload" : "unload";
+    TrackLib.Events.add(window, unload, TrackUI.flush);
+  },
+  /**
+   * Sets data for the first time for a given user.
+   */
+  initNewData: function() {
+    var win = TrackLib.Dimension.getWindowSize(), 
+        doc = TrackLib.Dimension.getDocumentSize(),
+        data  = "url="      + escape(window.location.href);
+        data += "&screenw=" + screen.width;
+        data += "&screenh=" + screen.height;
+        data += "&winw="    + win.width;
+        data += "&winh="    + win.height;
+        data += "&docw="    + doc.width;
+        data += "&doch="    + doc.height;
+        data += "&info="    + TrackUI.info;
+        data += "&action="  + "init";
+    // send request
+    TrackUI.send({
+      postdata: data, 
+      callback: TrackUI.setUserId
+    });
+    // clean up
+    TrackUI.info = [];
+  },
+  /**
+   * Sets the user ID, to append data for the same session.
+   * @return void
+   * @param {string} response  XHR response text
+   */
+  setUserId: function(response) {
+    TrackUI.uid = parseInt(response);
+    if (TrackUI.uid) {
+      setInterval(TrackUI.appendData, TrackUI.settings.postInterval*1000);
+    }
+  }, 
+  /**
+   * Continues saving data for the same (previous) user.
+   */
+  appendData: function() {
+    var data  = "uid="     + TrackUI.uid;
+        data += "&info="   + TrackUI.info;
+        data += "&action=" + "append";
+    // send request
+    TrackUI.send({
+      postdata: data
+    });
+    // clean up
+    TrackUI.info = [];  
+  }, 
+  /**
+   * A common sending method with CORS support.
+   */
+  send: function(req) {
+    req.url = TrackUI.settings.postServer;
+    req.async = false;
+    TrackLib.XHR.sendAjaxRequest(req);
+  }, 
+  /**
+   * Handles mouse events.
+   * @param {object}  e Event.
+   * @return void     
+   */
+  mouseHandler: function(e) {
+    TrackUI.eventHandler(e);    
+  }, 
+  /**
+   * Handles keyboard events.
+   * @param {object}  e Event.     
+   * @return void     
+   */    
+  keyHandler: function(e) {
+    TrackUI.eventHandler(e);    
+  },
+  /**
+   * Handles window events.
+   * @param {object}  e Event.     
+   * @return void     
+   */    
+  winHandler: function(e) {
+    TrackUI.eventHandler(e);
+  },  
+  /** 
+   * Generic callback for event listeners.
+   * @return void
+   */
+  eventHandler: function(e) {
+    e = TrackLib.Events.fix(e);
+    
+    var coords = TrackUI.getMousePos(e), elem = TrackUI.findElement(e);
+    TrackUI.fillInfo(coords.x, coords.y, e.type, elem);
+  }, 
+  /** 
+   * Callback for touch event listeners.
+   * @return void
+   */  
+  touchHandler: function(e) {
+    e = TrackLib.Events.fix(e);
+    
+    var touch = e.touches[0] || e.targetTouches[0]; // || e.changedTouches[0]
+    // remove (emulated) mouse events on mobile devices and bind them to mouse callbacks
+    switch(e.type) {
+      case "touchstart": 
+      case "touchmove":
+      case "touchend":
+        TrackLib.Events.remove(document, e.type, TrackUI.eventHandler);
+        TrackUI.eventHandler(touch);
+        break;
+      default: 
+        return;
+    }  
+  }, 
+  /** 
+   * Cross-browser way to register the mouse position.
+   * @return {object} Coordinates
+   *   @config {int} x Horizontal component
+   *   @config {int} y Vertical component
+   */
+  getMousePos: function(e) {
+    e = TrackLib.Events.fix(e);
+    
+    var cx = 0, cy = 0;
+    if (e.pageX || e.pageY) {
+      cx = e.pageX;
+      cy = e.pageY;
+    }	else if (e.clientX || e.clientY) {
+      cx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      cy = e.clientY + document.body.scrollTop  + document.documentElement.scrollTop;
+    }
+    // sometimes the mouse coordinates are negative (e.g., in Opera)
+    if (!cx || cx < 0) cx = 0;
+    if (!cy || cy < 0) cy = 0;
+
+  	return { x:cx, y:cy };
+  }, 
+  /** 
+   * Gets the interacted element.
+   * @return {string} XPath
+   */
+  findElement: function(e) {
+    e = TrackLib.Events.fix(e);
+    
+    return TrackLib.XPath.getXPath(e.target);
+  }, 
+  /** 
+   * Callback for touch event listeners.
+   * @param {int} x Cursor X position
+   * @param {int} y Cursor Y position
+   * @param {string} event    Related event
+   * @param {string} element  Related element
+   * @return void
+   */ 
+  fillInfo: function(x,y,event,element) {
+    TrackUI.info.push( new Date().getTime() +" "+ x +" "+ y +" "+ event +" "+ element );
+  },
+  /** 
+   * Transmit remaining (if any) data to server.
+   * @return void
+   */   
+  flush: function(e) {
+    if (TrackUI.uid) {
+      TrackUI.appendData();
+    } else {
+      TrackUI.initNewData();
+    }
+  }
+ 
+};
